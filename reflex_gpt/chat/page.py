ebca1aaf8fb9e8f129a295156252957a58c54d30
JayGhz <jaykansas7@gmail.com>
2024-12-30 01:31:43 -0500

    Add message handling and display in chat page

--- reflex_gpt/chat/page.py
+++ reflex_gpt/chat/page.py
@@ -2,7 +2,15 @@ import reflex as rx
 
 from reflex_gpt import ui
 
+from .state import ChatMessage, ChatState
 from .form import chat_form
+
+
+def message_box(chat_message: ChatMessage):
+    return rx.box(
+        rx.text(chat_message.message),
+        background_color=rx.cond(chat_message.is_bot, rx.color("gray"), rx.color("blue")),
+    )
 
 
 def chat_page():
@@ -10,7 +18,11 @@ def chat_page():
     return ui.base_layout(
         rx.vstack(
             rx.heading("Chat Here", size="9"),
-            chat_form(),    
+            rx.box(
+                rx.foreach(ChatState.messages, message_box),
+                width="100%",
+            ),
+            chat_form(),
             spacing="5",
             justify="center",
             min_height="85vh",
--- reflex_gpt/chat/state.py
+++ reflex_gpt/chat/state.py
@@ -1,18 +1,32 @@
+from typing import List
 import asyncio
 import reflex as rx
 
 
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool
+
+
 class ChatState(rx.State):
     did_submit: bool = False
+    messages: List[ChatMessage] = []
 
     @rx.var
     def user_did_submit(self) -> bool:
         return self.did_submit
+    
+    def append_message(self, message, is_bot=False):
+        self.messages.append(ChatMessage(is_bot=is_bot, message=message))
 
     async def handle_submit(self, form_data: dict):
         print(form_data)
-        self.did_submit = True
-        yield
-        await asyncio.sleep(2)
-        self.did_submit = False
-        yield
+        user_message = form_data.get("message", "")
+        if user_message:
+            self.did_submit = True
+            self.append_message(user_message, is_bot=False)
+            yield
+            await asyncio.sleep(2)
+            self.did_submit = False
+            self.append_message(user_message, is_bot=True)
+            yield
